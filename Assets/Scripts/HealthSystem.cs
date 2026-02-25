0de457aa6500e04158b63112a9d9ca20a214c38d
NANEDCA <dhernandezr@edu.tecnocampus.cat>
2026-02-25 11:34:25 +0100

    Healthbar

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public Slider healthSlider;  
+    
+    void Start()
+    {
+        healthSlider.value = 100;
+    }
+
+   public void QuitHealth(float damage)
+   { 
+
+        healthSlider.value -= damage;
+
+   } 
+    public void Heal(float life)
+    { 
+        if (healthSlider.value <=100)
+        healthSlider.value += life;
+    }
+
+}
